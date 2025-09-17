5654eadb0fa75211520a5f3b0086ad638cb9033d
Niketa <rudkewalniketa@gmail.com>
2025-09-17 16:37:21 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 dotenv.config({
     path: './env'
 });
-const app = express();
+
 //connection to DB
 connectDB();
-app.listen("8000",()=>{
-    console.log("server is listening");
-})
+
